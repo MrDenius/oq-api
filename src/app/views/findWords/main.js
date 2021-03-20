d3d88579e6ab704e339520f65372f446ecbf0266
Denius <denis_ovchinikov03@mail.ru>
2021-03-20 11:09:40 +0700

    push

--- src/app/views/findWords/main.js
+++ src/app/views/findWords/main.js
@@ -8,12 +8,46 @@ let pass = (canvas.height + 1) / 10;
 
 const Main = () => {
 	DrawSharp();
+	DrawWords(GenerateLetters(["слово"]));
+};
 
-	ctx.font = `${pass}px serif`;
-	ctx.textAlign = "center";
-	ctx.textBaseline = "middle"
+const GenerateLetters = (words) => {
+	const MAX_LETTERS = 9;
 
-	ctx.fillText("A",pass/2,pass/2)
+	const letters = "йцукенгшщзхъфывапролджэячсмитьбюё".split("");
+	const ltrs = [];
+
+	const RandLetter = () => {
+		let rand = 0 - 0.5 + Math.random() * (letters.length - 1 - 0 + 1);
+		return letters[Math.round(rand)];
+	};
+	for (let x = 0; x <= MAX_LETTERS; x++) {
+		ltrs[x] = [];
+		for (let y = 0; y <= MAX_LETTERS; y++) {
+			ltrs[x][y] = RandLetter();
+		}
+	}
+
+	return ltrs;
+};
+
+const GetLogicSharp = () => {};
+
+const DrawWords = (letter) => {
+	let XX = 0;
+	let YY = 0;
+	for (let y = pass / 2; y <= canvas.height; y += pass) {
+		for (let x = pass / 2; x <= canvas.width; x += pass) {
+			ctx.font = `${pass}px serif`;
+			ctx.textAlign = "center";
+			ctx.textBaseline = "middle";
+
+			ctx.fillText(letter[XX][YY] || "A", x, y);
+			XX++;
+		}
+		YY++;
+		XX = 0;
+	}
 };
 
 const DrawSharp = () => {
